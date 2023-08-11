1e7324ef24545491b44d34d8d39532b0e1d95b06
Hassan Lakhal <hlakhal-@e1r10p8.1337.ma>
2023-08-11 05:51:03 +0100

    add

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    n = len(sys.argv)
-    if n == 1:
-        print("{0} arguments.".format(n))
-    elif n > 1:
-        print("{0} arguments:".format(n - 1))
-        for i in range(1,n):
+    if len(sys.argv) == 1:
+        print("{0} arguments.".format(len(sys.argv)))
+    elif len(sys.argv) > 1:
+        print("{0} arguments:".format(len(sys.argv) - 1))
+        for i in range(1,len(sys.argv)):
             print("{0}: {1}".format(i,sys.argv[i]))
 
 
