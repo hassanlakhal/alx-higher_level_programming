e9016e50122317c3f64f778bbe21ed865d914a3d
Hassan Lakhal <hlakhal-@e1r2p6.1337.ma>
2023-08-12 01:31:54 +0100

    add

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,6 +7,3 @@ if __name__ == "__main__":
         print("{0} arguments:".format(len(sys.argv) - 1))
         for i in range(1, len(sys.argv)):
             print("{0}: {1}".format(i, sys.argv[i]))
-
-
-
