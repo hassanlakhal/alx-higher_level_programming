b1e595cb4262f3ba636671202f7797e6118ad9d0
hassanlakhal <lakehalhassan2120@gmail.com>
2023-08-30 23:05:33 +0000

    test

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,12 +2,7 @@
 
 
 class Square:
-    """
-    class square that has attributes:
-        size
-    """
-    def __init__(self, size):
-        """
-        the initialization function for the square class
-        """
+    """class square that has attributes:size"""
+    def __init__(self, size=0):
+        """ the initialization function for the square class"""
         self.__size = size
