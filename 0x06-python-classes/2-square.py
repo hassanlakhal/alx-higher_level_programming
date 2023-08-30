ab089fc632ddb56dc2a8c7c2f37e45af91af14f7
hassanlakhal <lakehalhassan2120@gmail.com>
2023-08-30 23:12:34 +0000

    fixed

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Defines a square bassed on 1-square.py"""
 
 
 class Square:
@@ -16,5 +17,4 @@ class Square:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = size
